520f5f4e5efd94cef36884c389c81582c7be6373
kmg <deepestdark@gmail.com>
2025-12-17 16:00:20 +0900

    code add

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,14 +1,30 @@
 class Todo {
-  int _id = 0;
-  String _todoTitle = '';
-  String _todoContent = '';
-  bool _isComplate = false;
-  DateTime _todoWriteDate = DateTime.now();
-  DateTime? _todoComplateDate = null;
+  int id;
+  final String todoTitle;
+  final String todoContent;
+  final bool isComplate;
+  final DateTime todoWriteDate;
+  DateTime? todoComplateDate;
 
-  String get title => _todoTitle;
-  String get content => _todoContent;
-  bool get complate => _isComplate;
-  DateTime get writeDate => _todoWriteDate;
-  DateTime? get complateDate => _todoComplateDate;
+  Todo({
+    required this.id,
+    required this.todoTitle,
+    required this.todoContent,
+    required this.isComplate,
+    required this.todoWriteDate,
+    this.todoComplateDate,
+  });
+
+  Todo setTodo({
+    required int id,
+    required String title,
+    required String content,
+    required bool isComplate,
+  }) => Todo(
+    id: id,
+    todoTitle: title,
+    todoContent: content,
+    isComplate: isComplate,
+    todoWriteDate: DateTime.now(),
+  );
 }
--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_ex01/models/todo.dart';
 
+/// TodoProvider는 할 일(todo) 데이터를 제공 및 관리하고,
+/// 상태 변경 시 리스너(위젯 등)에게 알리는 ChangeNotifier 클래스입니다.
 class TodoProvider extends ChangeNotifier {
+  /// 전체 할 일 목록을 저장하는 리스트입니다.
   List<Todo> todoList = [];
 
+  /// (기능 구현 예정) todo의 id를 증가시키는 메서드입니다.
   void incrementId() {}
 
+  /// 새로운 할 일(todo)을 [todoList]에 추가합니다.
+  /// 추가 성공 시 true, 실패 시 false를 반환합니다.
+  /// 추가 후 리스너에게 변경 사항을 알립니다.
   bool addToTodoList({required Todo todo}) {
     try {
       todoList.add(todo);
-      notifyListeners();
+      notifyListeners(); // 변경 알림
       return true;
     } catch (e) {
       return false;
     }
   }
 
+  /// 기존 할 일(todo)을 [todoList]에서 찾아 수정합니다.
+  /// 수정 성공 시 true, 실패 시 false를 반환합니다.
+  /// 수정 후 리스너에게 변경 사항을 알립니다.
   bool modifyToTodoList({required Todo todo}) {
+    bool result = false;
     try {
-      if (todoList.isNotEmpty) {
-        notifyListeners();
-        return true;
-      } else {
-        return false;
+      // 동일한 id를 갖는 항목의 인덱스를 찾습니다.
+      final int index = todoList.indexWhere((t) => t.id == todo.id);
+
+      // 해당 항목이 존재할 경우 리스트 값을 수정합니다.
+      if (index != -1) {
+        todoList[index] = todo;
+        notifyListeners(); // 변경 알림
+        result = true;
       }
     } catch (e) {
-      return false;
+      debugPrint(e.toString());
     }
+
+    return result;
   }
 
+  /// 할 일(todo)을 [todoList]에서 삭제합니다.
+  /// 삭제 성공 시 true, 실패 시 false를 반환합니다.
+  /// 삭제 후 리스너에게 변경 사항을 알립니다.
   bool deleteToTodoList({required Todo todo}) {
+    bool result = false;
     try {
-      if (todoList.isNotEmpty) {
-        notifyListeners();
-        return true;
-      } else {
-        return false;
-      }
+      // id가 일치하는 모든 항목을 삭제합니다.
+      todoList.removeWhere((item) => item.id == todo.id);
+      notifyListeners(); // 변경 알림
+      result = true;
     } catch (e) {
-      return false;
+      debugPrint(e.toString());
     }
+    return result;
   }
 }
